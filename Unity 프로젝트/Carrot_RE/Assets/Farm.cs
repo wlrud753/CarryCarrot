b94513be4e439d17d9a989ca1f28d613bbec0ae3
wlrud753 <jimmy2829@naver.com>
2020-10-18 01:56:29 +0900

    unity 프로젝트 추가

---
+++ Unity 프로젝트/Carrot_RE/Assets/Farm.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Farm : MonoBehaviour
+{
+    PlayerInfo playerInfo;
+
+    int FarmID;
+
+    int FarmLV;
+    public int FarmEarning;
+
+    Slider slider;
+    public float PlantTimer;
+    float timer;
+
+    GameObject FarmInfoWindow;
+
+    void Start()
+    {
+        playerInfo = GameObject.Find("Player Info").GetComponent<PlayerInfo>();
+        slider = this.transform.Find("Canvas").Find("Slider").GetComponent<Slider>();
+        FarmInfoWindow = GameObject.Find("Farm Info").transform.Find("Farm Info Window").gameObject;
+
+        timer = PlantTimer;
+
+        setSlider();
+    }
+    void setSlider()
+    {
+        slider.maxValue = PlantTimer;
+    }
+
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            timer = PlantTimer;
+            playerInfo.getCarrot(FarmEarning);
+        }
+
+        timer -= Time.deltaTime;
+        slider.value = timer;
+    }
+
+    private void OnMouseDown()
+    {
+        FarmInfoWindow.SetActive(true);
+    }
+}
---
+++ Unity 프로젝트/Carrot_RE/Assets/PlayerInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInfo : MonoBehaviour
+{
+    int Carrot;
+
+    public TextMeshProUGUI CarrotTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CarrotTxt = GameObject.Find("Info UI").transform.Find("Canvas").
+        //    Find("Carrot Text").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CarrotTxt.text = this.Carrot.ToString();
+    }
+
+    #region Carrot
+    public void getCarrot(int _amount)
+    {
+        this.Carrot += _amount;
+    }
+    public bool useCarrot(int _amount)
+    {
+        if(this.Carrot - _amount >= 0)
+        {
+            this.Carrot -= _amount;
+            return true;
+        }
+        return false;
+    }
+    #endregion
+}
